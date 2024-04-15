0899ba837d70d11ab71e6003cb847e44560f060b
Gabriel Garcia Wedig <gabriel.wedig@cwi.com.br>
2024-04-15 16:19:28 -0300

    CORS

--- Appointify.Api/Program.cs
+++ Appointify.Api/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:5173")
                .AllowAnyHeader()
                .AllowAnyMethod()
                .AllowCredentials();
