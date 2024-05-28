24bd9d137dd5495dda4bd6058c20b63d5f2074db
Gabriel Garcia Wedig <gabriel.wedig@cwi.com.br>
2024-05-28 16:22:29 -0300

    🚧 WiP

--- Appointify.Api/Program.cs
+++ Appointify.Api/Program.cs
@@ -98,15 +98,6 @@ app.UseAuthentication();
 
 app.UseAuthorization();
 
-var defaultCulture = new CultureInfo("pt-BR");
-var localizationOptions = new RequestLocalizationOptions
-{
-    DefaultRequestCulture = new RequestCulture(defaultCulture),
-    SupportedCultures = new List<CultureInfo> { defaultCulture },
-    SupportedUICultures = new List<CultureInfo> { defaultCulture }
-};
-app.UseRequestLocalization(localizationOptions);
-
 app.MapControllers();
 
 app.Run();
